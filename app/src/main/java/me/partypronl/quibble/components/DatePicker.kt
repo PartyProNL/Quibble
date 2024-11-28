780f978d3fd20203f01e104e925361d04f1903f5
Youri Scheepers <youri.scheepers@hva.nl>
2024-11-28 23:54:01 +0100

    Create re-usable date picker component

---
+++ app/src/main/java/me/partypronl/quibble/components/DatePicker.kt
@@ -0,0 +1,42 @@
+package me.partypronl.quibble.components
+
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDefaults
+import androidx.compose.material3.DatePickerDialog
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.SelectableDates
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.rememberDatePickerState
+import androidx.compose.runtime.Composable
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DatePicker(
+    onDateSelected: (Long?) -> Unit,
+    onDismiss: () -> Unit,
+    selectableDates: SelectableDates? = null
+) {
+    val datePickerState = rememberDatePickerState(
+        selectableDates = selectableDates ?: DatePickerDefaults.AllDates
+    )
+
+    DatePickerDialog(
+        onDismissRequest = onDismiss,
+        confirmButton = {
+            TextButton(onClick = {
+                onDateSelected(datePickerState.selectedDateMillis)
+                onDismiss()
+            }) {
+                Text("Confirm")
+            }
+        },
+        dismissButton = {
+            TextButton(onClick = onDismiss) {
+                Text("Cancel")
+            }
+        }
+    ) {
+        DatePicker(state = datePickerState)
+    }
+}
