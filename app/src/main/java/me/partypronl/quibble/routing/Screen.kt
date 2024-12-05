a23d2cb5647e654e091cd50f31293d359b554631
Youri Scheepers <youri.scheepers@hva.nl>
2024-12-05 20:31:07 +0100

    Remove unnecessary duplicate serializable annotations

--- app/src/main/java/me/partypronl/quibble/routing/Screen.kt
+++ app/src/main/java/me/partypronl/quibble/routing/Screen.kt
@@ -4,8 +4,8 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 sealed class Screen(val route: String) {
-    @Serializable object Home: Screen("home")
-    @Serializable object WriteTextEntry: Screen("write/text/{date}")
-    @Serializable object Search: Screen("search")
-    @Serializable object You: Screen("you")
+    object Home: Screen("home")
+    object WriteTextEntry: Screen("write/text/{date}")
+    object Search: Screen("search")
+    object You: Screen("you")
 }
