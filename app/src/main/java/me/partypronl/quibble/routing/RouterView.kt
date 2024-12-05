3b44a2c9084faab91a1cca80bef8d931ee76cbe9
Youri Scheepers <youri.scheepers@hva.nl>
2024-12-05 20:32:38 +0100

    Remove old unused routing files

--- app/src/main/java/me/partypronl/quibble/routing/RouterView.kt
+++
@@ -1,9 +0,0 @@
-package me.partypronl.quibble.routing
-
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.runtime.Composable
-
-@Composable
-fun RouterView(currentRoute: Route, innerPadding: PaddingValues) {
-    currentRoute.page(innerPadding)
-}
