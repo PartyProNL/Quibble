0b78b467f4f1174c2684b3a352463ebfe738ad33
Youri Scheepers <youri.scheepers@hva.nl>
2024-12-07 23:00:03 +0100

    Register DAO and entity to database and increase version number

--- app/src/main/java/me/partypronl/quibble/data/QuibbleDatabase.kt
+++ app/src/main/java/me/partypronl/quibble/data/QuibbleDatabase.kt
@@ -3,19 +3,23 @@ package me.partypronl.quibble.data
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import me.partypronl.quibble.data.dao.ColumnTypeDao
+import me.partypronl.quibble.data.dao.JournalColumnEntryDao
 import me.partypronl.quibble.data.dao.JournalEntryDao
 import me.partypronl.quibble.data.dao.JournalTextEntryDao
 import me.partypronl.quibble.data.models.ColumnTypeModel
+import me.partypronl.quibble.data.models.JournalColumnEntryModel
 import me.partypronl.quibble.data.models.JournalEntryModel
 import me.partypronl.quibble.data.models.JournalTextEntryModel
 
 @Database(entities = [
     JournalEntryModel::class,
     JournalTextEntryModel::class,
-    ColumnTypeModel::class
-], version = 2)
+    ColumnTypeModel::class,
+    JournalColumnEntryModel::class
+], version = 3)
 abstract class QuibbleDatabase: RoomDatabase() {
     abstract fun journalEntryDao(): JournalEntryDao
     abstract fun journalTextEntryDao(): JournalTextEntryDao
     abstract fun columnTypeDao(): ColumnTypeDao
+    abstract fun journalColumnEntryDao(): JournalColumnEntryDao
 }
